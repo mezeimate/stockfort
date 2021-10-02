7e0d9a5e03f77db8b7f41de5d9179e6e667a1093
Mezei Mate <mezeimate000@gmail.com>
2021-10-02 13:23:33 +0200

    add imports

--- src/main/java/stockapp/controllers/LogController.java
+++ src/main/java/stockapp/controllers/LogController.java
@@ -1,6 +1,15 @@
 package stockapp.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
 
 import java.io.IOException;
 
