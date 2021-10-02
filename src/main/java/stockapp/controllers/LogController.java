28bac9b8fa94990dbf04c222e65ed9a04ed103e1
Mezei Mate <mezeimate000@gmail.com>
2021-10-02 13:22:02 +0200

    create logcontoller

---
+++ src/main/java/stockapp/controllers/LogController.java
@@ -0,0 +1,13 @@
+package stockapp.controllers;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class LogController {
+
+
+    @FXML
+    private void handlelogButton(ActionEvent event) throws IOException {
+    }
+}
