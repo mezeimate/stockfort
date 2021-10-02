2bad1d0fd6fff2030fb065c62868cc9d3a637096
Konn25 <koncz.bence9@gmail.com>
2021-10-02 17:38:13 +0200

    added variables and constructor

--- src/main/java/stockapp/model/Raktar.java
+++ src/main/java/stockapp/model/Raktar.java
@@ -1,5 +1,24 @@
 package stockapp.model;
 
+/**
+ * A Raktar osztály valósítja meg a raktár megjelenítéséhez szükséges táblázat adatszerkezetét.
+ */
 public class Raktar {
 
+    /**
+     * A Raktar osztály konstruktora.
+     */
+
+    /**
+     * A Raktar osztály plédányait kiíró metódusa.
+     */
+    @Override
+    public String toString() {
+        return "tmp{" +
+                "nev='" + nevOszlop + '\'' +
+                ", termek='" + termekOszlop + '\'' +
+                ", mennyiseg=" + mennyisegOszlop +
+                ", datum=" + datumOszlop +
+                '}'+"\r\n";
+    }
 }
