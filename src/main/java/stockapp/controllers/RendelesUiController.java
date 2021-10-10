92b74a3468f8dc86013d7aa185b88489cb48fd12
Kovács Nikoletta <Niki0505@Citromail.hu>
2021-10-10 17:17:47 +0200

    add rendeles ui controller

---
+++ src/main/java/stockapp/controllers/RendelesUiController.java
@@ -0,0 +1,34 @@
+package stockapp.controllers;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+import lombok.extern.java.Log;
+import org.tinylog.Logger;
+import stockapp.model.Raktar;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * A program rendelés felvétele képernyőjének megvalósítása.
+ */
+public class RendelesUiController {
+
+}
